550798645cbc628f992a1cce2c9f6f871e75ed7c
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-09-27 17:19:38 +0300

    Add project files.

---
+++ Tyuiu.IvanovJD.Sprint0.Csharp.Task0.Lib/Tyuiu.IvanovJD.Sprint0.Csharp.Task0.Lib.cpp
@@ -0,0 +1,10 @@
+// Tyuiu.IvanovJD.Sprint0.Csharp.Task0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnTyuiuIvanovJDSprint0CsharpTask0Lib()
+{
+}
---
+++ Tyuiu.IvanovJD.Sprint0.Csharp.Task0.Test/Tyuiu.IvanovJD.Sprint0.Csharp.Task0.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuIvanovJDSprint0CsharpTask0Test
+{
+	TEST_CLASS(TyuiuIvanovJDSprint0CsharpTask0Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
