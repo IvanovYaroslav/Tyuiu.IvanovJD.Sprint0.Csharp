5e23a7c2797be5360e7e9245e5fff672be51c964
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-09-28 22:19:30 +0300

    Task1

---
+++ Tyuiu.IvanovJD.Sprint0.Csharp.Task1.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.IvanovJD.Sprint0.Csharp.Task1.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tyuiu.IvanovJD.Sprint0.Csharp.Task1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovJD.Sprint0.Csharp.Task1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovJD.Sprint0.Csharp.Task1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8018d9fe-b208-4450-aae6-c232af4193fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
