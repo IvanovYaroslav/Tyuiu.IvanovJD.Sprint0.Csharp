d86a7358ddbc110f88fd2addc5eae8e337776989
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-09-30 05:07:17 +0300

    Task5

---
+++ Tyuiu.IvanovJD.Sprint0.Csharp.Task5.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovJD.Sprint0.Csharp.Task5.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovJD.Sprint0.Csharp.Task5.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63987f97-8799-4f7f-9893-d6f6cc9f0502")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanovJD.Sprint0.Csharp.Task5.V1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovJD.Sprint0.Csharp.Task5.V1.Lib;
+
+namespace Tyuiu.IvanovJD.Sprint0.Csharp.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B" + DataService.Addition(10, 5));
+            Console.WriteLine("A - B" + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B" + DataService.Multiplication(17, 5));
+            Console.WriteLine("A / B" + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
